d62a6cd89d1f6c5ce9495da68e1221fdbbe7e4ea
Aryan <110285758+IAMARYANN@users.noreply.github.com>
2025-03-29 23:53:18 +0530

    Time: 12 ms (22.11%) | Memory: 131.7 MB (58.47%) - LeetSync

--- 2265-partition-array-according-to-given-pivot/partition-array-according-to-given-pivot.cpp
+++ 2265-partition-array-according-to-given-pivot/partition-array-according-to-given-pivot.cpp
@@ -1,26 +1,25 @@
 class Solution {
 public:
-   vector<int> pivotArray(vector<int>& nums, int pivot) {
-    int low = 0, equal = 0, high = 0;
+vector<int> pivotArray(vector<int>& nums, int pivot) {
+    int n = nums.size();
+    vector<int> temp(nums);  // Copy of original array
+    int low = 0;
 
-    // First pass: Count the number of elements in each category
-    for (int num : nums) {
-        if (num < pivot) low++;
-        else if (num == pivot) equal++;
-        else high++;
+    // First pass: Move elements < pivot to the beginning
+    for (int num : temp) {
+        if (num < pivot) nums[low++] = num;
     }
 
-    // Create a new array and place elements based on the count
-    vector<int> result(nums.size());
-    int i = 0, j = low, k = low + equal;
-
-    // Second pass: Place elements in correct order
-    for (int num : nums) {
-        if (num < pivot) result[i++] = num;
-        else if (num == pivot) result[j++] = num;
-        else result[k++] = num;
+    // Second pass: Move elements == pivot to the middle
+    for (int num : temp) {
+        if (num == pivot) nums[low++] = num;
     }
 
-    return result;
+    // Third pass: Move elements > pivot to the end
+    for (int num : temp) {
+        if (num > pivot) nums[low++] = num;
+    }
+    return nums;
 }
+
 };
