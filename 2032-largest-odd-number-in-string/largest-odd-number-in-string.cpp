db0d605f42f0348b3bb4b554b344bcdf121f44ed
Aryan <110285758+IAMARYANN@users.noreply.github.com>
2025-04-17 01:58:56 +0530

    Time: 0 ms (100.00%) | Memory: 17.7 MB (81.26%) - LeetSync

--- 2032-largest-odd-number-in-string/largest-odd-number-in-string.cpp
+++ 2032-largest-odd-number-in-string/largest-odd-number-in-string.cpp
@@ -2,13 +2,6 @@ class Solution {
 public:
     string largestOddNumber(string num) {
         int n=num.size();
-        vector<int>ans;
-        for(int i=0;i<n;i++){
-            int chari = num[i] - '0';
-            cout<<chari;
-            ans.push_back(chari);
-        }
-        reverse(ans.begin(),ans.end());
         for(int i=n-1;i>=0;i--){
             if((num[i] - '0') %2 != 0){
                 string part = num.substr(0, i + 1);
