b84548caaa363072d85e1ef7976c4ab6954b8770
Aryan <110285758+IAMARYANN@users.noreply.github.com>
2025-04-25 17:52:03 +0530

    Time: 0 ms (100.00%) | Memory: 16.5 MB (31.15%) - LeetSync

--- 61-rotate-list/rotate-list.cpp
+++ 61-rotate-list/rotate-list.cpp
@@ -9,29 +9,26 @@
  * };
  */
 class Solution {
-public:
+public: 
     ListNode* rotateRight(ListNode* head, int k) {
-       if (head == nullptr || head->next == nullptr || k == 0) return head;
-        ListNode* temp=head;
-        vector<int>ans;
-        while(temp!=nullptr){
-            ans.push_back(temp->val);
-            temp=temp->next;
-        }
-        int n=ans.size();
-        k=k%n;
-        vector<int>ans1(n);
-        for(int i=0;i<ans.size();i++){
-            int a =(i+k)%n;
-            ans1[a]=ans[i];
-        }
-        ListNode* dummy=new ListNode(ans1[0]);
-        temp=dummy;
-        for(int i=1;i<n;i++){
-            ListNode* hehe =new ListNode(ans1[i]);
-            temp->next=hehe;
-            temp=hehe;
-        }
-        return dummy;
+       if(head == nullptr || head->next==nullptr || k==0)return head;
+       int len=1;
+       ListNode* tail=head;
+       while(tail->next!=nullptr){
+          len++;
+          tail=tail->next;
+       }
+       tail->next=head;
+       k=k%len;
+       int a =len-k-1;
+       ListNode *newHead=head;
+       while(a){
+          newHead=newHead->next;
+           a--;
+       }
+       ListNode *hehe=newHead->next;
+       newHead->next=nullptr;
+
+       return hehe;
     }
 };
