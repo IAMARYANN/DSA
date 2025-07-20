ebad0828402b32da352755f7fbbb435edd2cda59
Aryan <110285758+IAMARYANN@users.noreply.github.com>
2025-07-20 14:24:49 +0530

    Time: 47 ms (6.11%) | Memory: 41.7 MB (6.30%) - LeetSync

---
+++ 966-binary-subarrays-with-sum/binary-subarrays-with-sum.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    int numSubarraysWithSum(vector<int>& nums, int goal) {
+        int n=nums.size();
+        int cnt=0;
+        int pre_sum=0;
+        map<int,int>freq;
+        freq[0]=1;
+        for(int i=0;i<n;i++){
+            pre_sum+=nums[i];
+            int remove=pre_sum-goal;
+            cnt=cnt+freq[remove];
+            freq[pre_sum]+=1;
+        }
+        return cnt;
+    }
+};
